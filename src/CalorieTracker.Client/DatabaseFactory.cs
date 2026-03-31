9b02fdd1ef44a4d3b94036f4b2e94ed5c4ee1261
Mikayel Mkrtchyan <Mikayel.Mkrtchyan@ameriabank.am>
2026-03-31 08:51:04 +0400

    cs

--- src/CalorieTracker.Client/DatabaseFactory.cs
+++ src/CalorieTracker.Client/DatabaseFactory.cs
@@ -10,7 +10,7 @@ public class DatabaseFactory : IDesignTimeDbContextFactory<DatabaseContext>
         var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
 
         optionsBuilder.UseSqlServer(
-            "Data Source=SRV-SQL16-2D;Initial Catalog=db_myCar_Internship;User ID=usr_myCar_Internship;Password=75x9Ec4ECn5hS7ScawLnNEE4;Trusted_Connection=False;Encrypt=True;TrustServerCertificate=True");
+            "");
         return new DatabaseContext(optionsBuilder.Options);
     }
 }
